1e1720d4597e98675f4d99953a011be1008ef1b6
Ilija Matoski <ilijamt@gmail.com>
2024-12-12 00:49:11 +0100

    Update gitlab import

--- type_access_level.go
+++ type_access_level.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"slices"
 
-	"gitlab.com/gitlab-org/api/client-go"
+	g "gitlab.com/gitlab-org/api/client-go"
 )
 
 type AccessLevel string
@@ -69,19 +69,19 @@ func (i AccessLevel) String() string {
 func (i AccessLevel) Value() int {
 	switch i {
 	case AccessLevelNoPermissions:
-		return int(gitlab.NoPermissions)
+		return int(g.NoPermissions)
 	case AccessLevelMinimalAccessPermissions:
-		return int(gitlab.MinimalAccessPermissions)
+		return int(g.MinimalAccessPermissions)
 	case AccessLevelGuestPermissions:
-		return int(gitlab.GuestPermissions)
+		return int(g.GuestPermissions)
 	case AccessLevelReporterPermissions:
-		return int(gitlab.ReporterPermissions)
+		return int(g.ReporterPermissions)
 	case AccessLevelDeveloperPermissions:
-		return int(gitlab.DeveloperPermissions)
+		return int(g.DeveloperPermissions)
 	case AccessLevelMaintainerPermissions:
-		return int(gitlab.MaintainerPermissions)
+		return int(g.MaintainerPermissions)
 	case AccessLevelOwnerPermissions:
-		return int(gitlab.OwnerPermissions)
+		return int(g.OwnerPermissions)
 	}
 
 	return -1
