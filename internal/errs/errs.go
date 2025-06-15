f54fed9d512dea80c2007a14282a8d3ca18ebf11
Ilija Matoski <ilijamt@gmail.com>
2025-06-15 21:38:08 +0200

    cleanup and add some go docs

--- internal/errs/errs.go
+++ internal/errs/errs.go
@@ -3,9 +3,18 @@ package errs
 import "errors"
 
 var (
-	ErrNilValue             = errors.New("nil value")
-	ErrInvalidValue         = errors.New("invalid value")
-	ErrFieldRequired        = errors.New("required field")
-	ErrFieldInvalidValue    = errors.New("invalid value for field")
+	// ErrNilValue represents an error indicating a nil value was encountered where it is not allowed.
+	ErrNilValue = errors.New("nil value")
+
+	// ErrInvalidValue indicates that an operation encountered a value that is considered invalid or inappropriate.
+	ErrInvalidValue = errors.New("invalid value")
+
+	// ErrFieldRequired represents an error when a required field is missing
+	ErrFieldRequired = errors.New("required field")
+
+	// ErrFieldInvalidValue represents an error when a field contains an invalid value
+	ErrFieldInvalidValue = errors.New("invalid value for field")
+
+	// ErrBackendNotConfigured represents an error when trying to use a backend that hasn't been properly configured
 	ErrBackendNotConfigured = errors.New("backend not configured")
 )
--- internal/gitlab/type.go
+++ internal/gitlab/type.go
@@ -1,40 +1,51 @@
 package gitlab
 
 import (
-	"errors"
 	"fmt"
 	"slices"
+
+	"github.com/ilijamt/vault-plugin-secrets-gitlab/internal/errs"
 )
 
+// Type defines a string-based type to represent specific categories or modes, such as "saas" or "self-managed". This is the Gitlab Type
 type Type string
 
 const (
-	TypeSaaS        Type = "saas"
-	TypeDedicated   Type = "dedicated"
+	// TypeSaaS represents the "saas" type, indicating the software-as-a-service mode for GitLab deployments.
+	TypeSaaS Type = "saas"
+	// TypeDedicated represents the "dedicated" type, indicating a dedicated mode for GitLab deployments.
+	TypeDedicated Type = "dedicated"
+	// TypeSelfManaged represents the "self-managed" type, indicating a self-hosted mode for GitLab deployments.
 	TypeSelfManaged Type = "self-managed"
-	TypeUnknown          = Type("")
+
+	// TypeUnknown represents an uninitialized or unknown GitLab deployment type, used as a default fallback value.
+	TypeUnknown = Type("")
 )
 
 var (
-	ErrUnknownType = errors.New("unknown gitlab type")
+	ErrUnknownType = fmt.Errorf("%s: gitlab type", errs.ErrInvalidValue)
 
-	ValidGitlabTypes = []string{
+	validGitlabTypes = []string{
 		TypeSaaS.String(),
 		TypeSelfManaged.String(),
 		TypeDedicated.String(),
 	}
 )
 
+// String converts the Type value to its underlying string representation.
 func (i Type) String() string {
 	return string(i)
 }
 
+// Value returns the string representation of the Type by invoking the String method.
 func (i Type) Value() string {
 	return i.String()
 }
 
+// TypeParse attempts to parse the given string into a valid GitLab Type.
+// Returns the corresponding Type and nil error if successful, or TypeUnknown and an error if parsing fails.
 func TypeParse(value string) (Type, error) {
-	if slices.Contains(ValidGitlabTypes, value) {
+	if slices.Contains(validGitlabTypes, value) {
 		return Type(value), nil
 	}
 	return TypeUnknown, fmt.Errorf("failed to parse '%s': %w", value, ErrUnknownType)
