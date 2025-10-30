129c8aea15fd8121c8935381e354e364a3acaef2
Ilija Matoski <ilijamt@gmail.com>
2025-10-30 09:44:26 +0100

    cleanup models

--- internal/model/model.go
+++ internal/model/model.go
@@ -1,14 +1,5 @@
 package model
 
-// ExtendedModel defines an interface for objects that contain a model and
-// methods for collecting data relevant to model-based operations.
-type ExtendedModel interface {
-	Named
-	IsNil
-	Event
-	LogicalResponseData
-}
-
 type Named interface {
 	// GetName returns the model's name as a string
 	GetName() string
@@ -24,4 +15,9 @@ type Event interface{}
 type LogicalResponseData interface {
 	// LogicalResponseData returns a map containing relevant data that can be used in template operations or logical evaluations
 	LogicalResponseData() map[string]any
+}
+
+type LogicalResponseDataWithOptions[T any] interface {
+	// LogicalResponseData returns a map containing relevant data that can be used in template operations or logical evaluations
+	LogicalResponseData(opts T) map[string]any
 }
