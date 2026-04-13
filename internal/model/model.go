05febb10bfb05d3a4fe1eb760102c54092564303
Ilija Matoski <ilijamt@gmail.com>
2026-04-13 03:52:18 +0200

    refactor(model): removed unused Event interface from model code
    - Removed unused Event interface for simplification.

--- internal/model/model.go
+++ internal/model/model.go
@@ -9,9 +9,6 @@ type IsNil interface {
 	// IsNil returns a boolean indicating whether the instance is considered nil or invalid
 	IsNil() bool
 }
-
-type Event interface{}
-
 type LogicalResponseData interface {
 	// LogicalResponseData returns a map containing relevant data that can be used in template operations or logical evaluations
 	LogicalResponseData() map[string]any
