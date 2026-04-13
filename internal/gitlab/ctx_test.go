a92625a79d611879150fc21469872634e1cb54cd
Ilija Matoski <ilijamt@gmail.com>
2026-04-13 03:37:50 +0200

    test(gitlab): verified Gitlab client retrieval from context
    - Added test for Gitlab client retrieval from context.

--- internal/gitlab/ctx_test.go
+++ internal/gitlab/ctx_test.go
@@ -8,9 +8,21 @@ import (
 	"github.com/ilijamt/vault-plugin-secrets-gitlab/internal/gitlab"
 )
 
+type stubClient struct {
+	gitlab.Client
+}
+
 func TestEmptyGitlabClientFromContext(t *testing.T) {
 	ctx := gitlab.ClientNewContext(t.Context(), nil)
 	c, ok := gitlab.ClientFromContext(ctx)
 	require.False(t, ok)
 	require.Nil(t, c)
+}
+
+func TestGitlabClientFromContext(t *testing.T) {
+	want := &stubClient{}
+	ctx := gitlab.ClientNewContext(t.Context(), want)
+	got, ok := gitlab.ClientFromContext(ctx)
+	require.True(t, ok)
+	require.Same(t, want, got)
 }
