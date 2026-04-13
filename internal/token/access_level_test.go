6dcc8514450b7d99236793f87b28f123bef400f3
Ilija Matoski <ilijamt@gmail.com>
2026-04-13 04:09:33 +0200

    test(token): reorganized access level tests with subtests for clarity
    - Refactored access level tests to use subtests for clarity.

--- internal/token/access_level_test.go
+++ internal/token/access_level_test.go
@@ -10,39 +10,48 @@ import (
 
 func TestAccessLevel(t *testing.T) {
 	var tests = []struct {
+		name     string
 		expected gitlab.AccessLevel
 		input    string
 		err      bool
 	}{
 		{
+			name:     "owner",
 			expected: gitlab.AccessLevelOwnerPermissions,
 			input:    gitlab.AccessLevelOwnerPermissions.String(),
 		},
 		{
+			name:     "reporter",
 			expected: gitlab.AccessLevelReporterPermissions,
 			input:    gitlab.AccessLevelReporterPermissions.String(),
 		},
 		{
+			name:     "maintainer",
 			expected: gitlab.AccessLevelMaintainerPermissions,
 			input:    gitlab.AccessLevelMaintainerPermissions.String(),
 		},
 		{
+			name:     "developer",
 			expected: gitlab.AccessLevelDeveloperPermissions,
 			input:    gitlab.AccessLevelDeveloperPermissions.String(),
 		},
 		{
+			name:     "guest",
 			expected: gitlab.AccessLevelGuestPermissions,
 			input:    gitlab.AccessLevelGuestPermissions.String(),
 		},
 		{
+			name:     "no_permissions",
 			expected: gitlab.AccessLevelNoPermissions,
 			input:    gitlab.AccessLevelNoPermissions.String(),
 		},
 		{
+			name:     "minimal_access",
 			expected: gitlab.AccessLevelMinimalAccessPermissions,
 			input:    gitlab.AccessLevelMinimalAccessPermissions.String(),
 		},
 		{
+			name:     "unknown",
 			expected: gitlab.AccessLevelUnknown,
 			input:    "unknown",
 			err:      true,
@@ -50,15 +59,16 @@ func TestAccessLevel(t *testing.T) {
 	}
 
 	for _, test := range tests {
-		t.Logf("assert parse(%s) = %s (err: %v)", test.input, test.expected, test.err)
-		val, err := gitlab.AccessLevelParse(test.input)
-		assert.EqualValues(t, test.expected, val)
-		if test.err {
-			assert.ErrorIs(t, err, gitlab.ErrUnknownAccessLevel)
-			assert.Less(t, val.Value(), 0)
-		} else {
-			assert.NoError(t, err)
-			assert.GreaterOrEqual(t, val.Value(), 0)
-		}
+		t.Run(test.name, func(t *testing.T) {
+			val, err := gitlab.AccessLevelParse(test.input)
+			assert.EqualValues(t, test.expected, val)
+			if test.err {
+				assert.ErrorIs(t, err, gitlab.ErrUnknownAccessLevel)
+				assert.Less(t, val.Value(), 0)
+			} else {
+				assert.NoError(t, err)
+				assert.GreaterOrEqual(t, val.Value(), 0)
+			}
+		})
 	}
 }
